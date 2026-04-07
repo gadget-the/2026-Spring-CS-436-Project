b44dae7cdcca21cb122ebb26fb5c54d8baccea90
gadget-the <96590338+gadget-the@users.noreply.github.com>
2026-04-06 19:28:47 -0700

    added try catch to message send and receive

--- Client/client-class.py
+++ Client/client-class.py
@@ -1,3 +1,4 @@
+import json
 from socket import socket, AF_INET, SOCK_DGRAM
 
 class socket_client():
@@ -13,7 +14,17 @@ class socket_client():
         self.clientSocket = socket(AF_INET, SOCK_DGRAM)
 
     def send_message(self, message = ""):
-        self.clientSocket.sendto(message.encode(),(self.serverName, self.serverPort))
+        try:
+            self.clientSocket.sendto(message.encode(), (self.serverName, self.serverPort))
+        except Exception as e:
+            self.stop()
+            print("message not succesful, error returned :", e)
+
+    def send_dict(self, data):
+        data_string = json.dumps(data) # serialize the data (turn it into a string)
+        # data_loaded = json.loads(data) # load the data (turn it back into a dict)
+
+        self.clientSocket.sendto(data_string.encode(), (self.serverName, self.serverPort))
 
     def receive_message(self):
         modifiedMessage, serverAddress = self.clientSocket.recvfrom(2048)
@@ -27,6 +38,7 @@ if __name__ == "__main__":
 
     message = input('Input lowercase sentence:')
     client1.send_message(message)
+    # client1.send_dict({"test": True})
 
     client1.stop()
 
--- Server/server-class.py
+++ Server/server-class.py
@@ -1,4 +1,4 @@
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM#, SOL_SOCKET, SO_REUSEADDR
 
 class socket_server():
     # serverPort = 12000
@@ -8,11 +8,11 @@ class socket_server():
     SERVER_CLOSE_MESSAGE = "STOP"
     SERVER_CLOSE_ACK = "CLOSE_ACK"
 
-    def __init__(self, given_port = 12000, given_socket = socket(AF_INET, SOCK_DGRAM)):
+    def __init__(self, given_port = 12000):
         # self.serverPort = 12000
         # self.serverSocket = socket(AF_INET, SOCK_DGRAM)
         self.serverPort = given_port
-        self.serverSocket = given_socket
+        self.serverSocket = socket(AF_INET, SOCK_DGRAM)
 
     # def __del__(self):
     #     self.stop()
@@ -24,21 +24,27 @@ class socket_server():
         self.serverSocket.close() # close the socket; might not unbind the port...
 
     def start(self):
+        # self.serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # https://docs.python.org/3.3/library/socket.html and https://docs.python.org/3.3/library/socket.html
         self.serverSocket.bind(('', self.serverPort)) # bind the socket to the port specified(12000)
 
         print ('The server is ready to receive')
 
         while True:
-            message, clientAddress = self.serverSocket.recvfrom(2048) # 
-            decoded_message = message.decode() # decode the message received from the client
+            try:
+                message, clientAddress = self.serverSocket.recvfrom(2048) # 
+                decoded_message = message.decode() # decode the message received from the client
 
-            if decoded_message == self.SERVER_CLOSE_MESSAGE: # check for close message
-                self.serverSocket.sendto(self.SERVER_CLOSE_ACK.encode(), clientAddress) # send ACK to client; want to have this in stop fxn, but not sure how w/o making clientaddress (more) global...
+                if decoded_message == self.SERVER_CLOSE_MESSAGE: # check for close message
+                    self.serverSocket.sendto(self.SERVER_CLOSE_ACK.encode(), clientAddress) # send ACK to client; want to have this in stop fxn, but not sure how w/o making clientaddress (more) global...
+                    self.stop()
+                    break
+
+                modifiedMessage = decoded_message.upper() # change message to uppercase
+                self.serverSocket.sendto(modifiedMessage.encode(), clientAddress) # send the modified message back to the client
+            except Exception as e:
                 self.stop()
+                print("message not succesful, error returned :", e)
                 break
-
-            modifiedMessage = decoded_message.upper() # change message to uppercase
-            self.serverSocket.sendto(modifiedMessage.encode(), clientAddress) # send the modified message back to the client
 
 if __name__ == "__main__":
     server1 = socket_server()
